b1e081c5359684273b20cf2f909ebe3ef1995416
Benjy Levin <benjylevin@college.harvard.edu>
2014-08-27 18:26:14 +0800

    first real commit of basic layout

---
+++ js/custom.js
@@ -0,0 +1,38 @@
+function animationClick(trigger, element, animation){
+element = $(element);
+trigger = $(trigger);
+trigger.click(
+    function() {
+        element.addClass('animated ' + animation);          
+        //wait for animation to finish before removing classes
+/*        window.setTimeout( function(){
+            element.removeClass('animated ' + animation);
+        }, 2000);       */    
+
+    })};
+
+animationClick("#pabout","#pabout", "tada");
+animationClick("#pabout","#about", "bounceInRight");
+
+    d3.selectAll("#pabout").on("click", function(){
+	d3.selectAll("#about").style("display","block");
+	d3.selectAll("#pabout").style("cursor","default");
+});
+
+
+animationClick("#presume","#presume", "tada");
+animationClick("#presume","#resume", "bounceInRight");
+
+    d3.selectAll("#presume").on("click", function(){
+	d3.selectAll("#resume").style("display","block");
+	d3.selectAll("#presume").style("cursor","default");
+});
+
+animationClick("#pcourses","#pcourses", "tada");
+animationClick("#pcourses","#courses", "bounceInRight");
+
+    d3.selectAll("#pcourses").on("click", function(){
+	d3.selectAll("#courses").style("display","block");
+	d3.selectAll("#pcourses").style("cursor","default");
+});
+
